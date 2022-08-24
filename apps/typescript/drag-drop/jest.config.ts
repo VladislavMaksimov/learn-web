87d0b8c615d09def97709a25935938e90709b6a0
Vladislav Maksimov <workmaximov@yandex.ru>
2022-08-24 11:49:23 +0500

    generate typescript drag-drop app

---
+++ apps/typescript/drag-drop/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'typescript-drag-drop',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/typescript/drag-drop'
+};
