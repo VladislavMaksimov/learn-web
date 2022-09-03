beff34674afe716fffe89770c8bd3856da57f1f6
Vladislav Maksimov <workmaximov@yandex.ru>
2022-09-03 15:03:00 +0500

    components-design: add eslint and prettier

--- apps/react/components-design/src/components/PieChart/PieChart.tsx
+++ apps/react/components-design/src/components/PieChart/PieChart.tsx
@@ -1,23 +1,25 @@
-import { FC } from "react";
-import { Cell, Pie, PieChart as RePieChart, PieProps } from "recharts"
+import { FC } from 'react';
+import { Cell, Pie, PieChart as RePieChart, PieProps } from 'recharts';
 
 export interface IPieData {
-    key: string;
-    value: number;
-    color: string;
+  key: string;
+  value: number;
+  color: string;
 }
 
 interface PieChartProps extends Partial<PieProps> {
-    size: number;
-    data?: IPieData[];
+  size: number;
+  data?: IPieData[];
 }
 
 export const PieChart: FC<PieChartProps> = ({ size, data }) => {
-    return (
-        <RePieChart width={size} height={size}>
-            <Pie dataKey={"value"} data={data}>
-                {data?.map(d => (<Cell key={d.key} fill={d.color}/>))}
-            </Pie>
-        </RePieChart>
-    );
-}
+  return (
+    <RePieChart width={size} height={size}>
+      <Pie dataKey={'value'} data={data}>
+        {data?.map((d) => (
+          <Cell key={d.key} fill={d.color} />
+        ))}
+      </Pie>
+    </RePieChart>
+  );
+};
